e84bc521f8b730aa8c15353f94ad90fa3e2c42fc
amaresh pattanaik <pattanaikamaresh@gmail.com>
2021-05-28 13:47:35 +0530

    Added product controller and model

---
+++ PromotionCoding/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionCoding.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Products()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ PromotionCoding/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionCoding.Models
+{
+    public class Product
+    {
+        public string SKUID { get; set; }
+        public int Price { get; set; }
+        public int Qty { get; set; }
+    }
+}
